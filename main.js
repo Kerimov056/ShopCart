fe010556f39b38fb465292a44f826f38895a67c8
Kerimov056 <ulvisk@code.edu.az>
2023-05-13 14:58:05 -0700

    cartArray

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var shop = (function(){
+var shop = (function () {
     cart = [];
     let obj = {};
 
@@ -9,35 +9,35 @@ var shop = (function(){
     }
 
     function dowland() {
-            sessionStorage.setItem('ShopCarts',JSON.stringify(cart))
+        sessionStorage.setItem('ShopCarts', JSON.stringify(cart))
     }
 
     function Cekmek() {
-            cart = JSON.parse(sessionStorage.getItem("ShopCarts"))
+        cart = JSON.parse(sessionStorage.getItem("ShopCarts"))
     }
-    if(sessionStorage.getItem("ShopCarts") != null){
+    if (sessionStorage.getItem("ShopCarts") != null) {
         Cekmek();
     }
 
 
     //Sebete elave etmek
-    obj.addToCart = function (name,price,count) {
-        for(var item in cart){
+    obj.addToCart = function (name, price, count) {
+        for (var item in cart) {
             if (cart[item].name = name) {
                 cart[item].count++;
                 dowland();
                 return;
             }
         }
-        var item = new Carts(name,price,count);
+        var item = new Carts(name, price, count);
         cart.push(item)
         dowland()
     }
     //sebetden cixarmaq
-    obj.removeCart = function() {
-        for(var item in cart){
-            if(cart[item].name === name){
-                if(cart[item].count==0){
+    obj.removeCart = function () {
+        for (var item in cart) {
+            if (cart[item].name === name) {
+                if (cart[item].count == 0) {
                     return;
                 }
                 cart[item].count--;
@@ -47,9 +47,9 @@ var shop = (function(){
     }
 
     //burda hepsini silme islemini goruruk
-    obj.removeAllCart = function() {
-        for(var item in cart){
-            if(cart[item].name === name){
+    obj.removeAllCart = function () {
+        for (var item in cart) {
+            if (cart[item].name === name) {
                 cart[item].count === 0;
                 break;
             }
@@ -58,43 +58,85 @@ var shop = (function(){
     }
 
     //HAMISIN BIRDEN SILME
-    obj.clearCart =  function() {
+    obj.clearCart = function () {
         cart = [];
         dowland();
     }
 
     //Cartin qiymetinin hesabi
-    obj.sumCart = function() {
+    obj.sumCart = function () {
         let sum = 0;
-        for(var item in cart){
+        for (var item in cart) {
             if (cart[item].name === name) {
-                sum = cart[item].price* cart[item].count;
+                sum = cart[item].price * cart[item].count;
             }
             return Number(sum);
         }
     }
 
     //cartlarin sayi
-    obj.sumCount = function() {
+    obj.sumCount = function () {
         let sum = 0;
-        for(var item in cart) {
+        for (var item in cart) {
             sum += cart[item].count;
-          }
-          return sum;
+        }
+        return sum;
     }
 
     //carttlar
-    obj.carts = function() {
+    obj.carts = function () {
         let newCart = [];
-        for(i in cart){
+        for (i in cart) {
             itemCopy = {};
             item = cart[i]
-            for(nn in item){
-                itemCopy[nn] = item [nn] 
+            for (nn in item) {
+                itemCopy[nn] = item[nn]
             }
-        }   
+            itemCopy.sum = Number(item.price * item.count);
+            newCart.push(itemCopy)
+        }
+        return newCart;
     }
 
 
     return obj;
 })();
+
+
+
+
+function displayCart() {
+    var cartlist = shoppingCart.listCart();
+    var exit = "";
+    for(var i in cartArray) {
+        let tr = document.createElement("tr")
+
+    let td = document.createElement("td")
+    td.textContent = cartlist[i].name
+    let td1 = document.createElement("td")
+    td1.textContent = cartlist[i].price
+    let td2 = document.createElement("td")  //3cu hisse
+    let div = document.createElement("div") //div start
+    div.className='input-group'
+    let btn = document.createElement("button")
+    btn.className='minus-item input-group-addon btn btn-primary'
+    btn.textContent = "-";
+    let input = document.createElement("input")
+    input.type = 'number'
+    input.className = 'item-count form-control'
+    input.value = cartArray[i].count
+    let btn2 = document.createElement("button")
+    btn2.className='plus-item btn btn-primary input-group-addon'
+    btn2.textContent="+"  //div bitdi td bitdi
+    let td3 = document.createElement("td")  //td start
+    let btn3 = document.createElement("button")
+    btn3.className = 'delete-item btn btn-danger'
+    btn3.textContent = "X"
+    let td4 = document.createElement("td")
+    td4.textContent = cartlist[i].sum   //tr end
+    }
+
+}
+
+const showCart = document.querySelector()
+
