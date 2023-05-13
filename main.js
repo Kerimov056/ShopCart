799dc170d4cd807be94e395710e0c421eeef5646
Kerimov056 <ulvisk@code.edu.az>
2023-05-13 16:44:22 -0700

    addToCart

--- main.js
+++ main.js
@@ -163,7 +163,7 @@ function creatCart() {
 
 }
 
-const showCart = document.querySelector("add-to-cart")
+const showCart = document.querySelector(".add-to-cart")
 
 
 //- buttonuna vurdgumuzda sayin azalmasi
@@ -187,14 +187,40 @@ showCartElements.addEventListener("click", function (e) {
 
 
 // sebete add elemek
-showCart.addEventListener("click", function (e) {
-    e.preventDefault()
-    let name = this.getAttribute("data-name")
-    let price = this.getAttribute("data-price")
-    shop.addToCart(name,price,1)
-    creatCart()
-})
+// showCart.addEventListener("click", function (e) {
+//     e.preventDefault()
+//     // let name = this.getAttribute("data-name")
+//     // let price = this.getAttribute("data-price")
+//     shop.addToCart(name,price,1)
+//     creatCart()
+// })
 
+function displayCart() {
+    var cartArray = shoppingCart.listCart();
+    var output = "";
+    for(var i in cartArray) {
+      output += "<tr>"
+        + "<td>" + cartArray[i].name + "</td>" 
+        + "<td>(" + cartArray[i].price + ")</td>"
+        + "<td><div class='input-group'><button class='minus-item input-group-addon btn btn-primary' data-name=" + cartArray[i].name + ">-</button>"
+        + "<input type='number' class='item-count form-control' data-name='" + cartArray[i].name + "' value='" + cartArray[i].count + "'>"
+        + "<button class='plus-item btn btn-primary input-group-addon' data-name=" + cartArray[i].name + ">+</button></div></td>"
+        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + " = " 
+        + "<td>" + cartArray[i].total + "</td>" 
+        +  "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingCart.totalCart());
+    $('.total-count').html(shoppingCart.totalCount());
+  }
+  $('.add-to-cart').click(function(event) {
+    event.preventDefault();
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+    shop.addToCart(name, price, 1);
+    displayCart();
+  });
 
 //Clear etmek 
 showCart.addEventListener("click", function (e) {
@@ -202,3 +228,7 @@ showCart.addEventListener("click", function (e) {
     creatCart()
 })
 
+
+// //count 
+// showCartElements.addEventListener("click",)
+// creatCart();
