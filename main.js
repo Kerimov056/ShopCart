99aacfdc8f1c7643f5c6562b0aa2d42958730004
Kerimov056 <ulvisk@code.edu.az>
2023-05-13 13:35:42 -0700

    CartClear,CartRemaove

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
-const showcart = document.querySelector('.show-cart')
+var shop = (function(){
+    cart = [];
+    let obj = {};
+
+    function Carts(name, price, count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    function dowland() {
+            sessionStorage.setItem('ShopCarts',JSON.stringify(cart))
+    }
+
+    function Cekmek() {
+            cart = JSON.parse(sessionStorage.getItem("ShopCarts"))
+    }
+    if(sessionStorage.getItem("ShopCarts") != null){
+        Cekmek();
+    }
+
+
+    //Sebete elave etmek
+    obj.addToCart = function (name,price,count) {
+        for(var item in cart){
+            if (cart[item].name = name) {
+                cart[item].count++;
+                dowland();
+                return;
+            }
+        }
+        var item = new Carts(name,price,count);
+        cart.push(item)
+        dowland()
+    }
+    //sebetden cixarmaq
+    obj.removeCart = function() {
+        for(var item in cart){
+            if(cart[item].name === name){
+                if(cart[item].count==0){
+                    return;
+                }
+                cart[item].count--;
+            }
+        }
+        dowland();
+    }
+
+    //burda hepsini silme islemini goruruk
+    obj.removeAllCart = function() {
+        for(var item in cart){
+            if(cart[item].name === name){
+                cart[item].count === 0;
+                break;
+            }
+        }
+        dowland()
+    }
+
+    //HAMISIN BIRDEN SILME
+    obj.clearCart =  function() {
+        cart = [];
+        dowland();
+    }
+})
