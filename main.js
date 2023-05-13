e2a9bb1e661b79b1a68486580e19d4e31101c2a2
Kerimov056 <ulvisk@code.edu.az>
2023-05-13 16:12:37 -0700

    clear items add basket

--- main.js
+++ main.js
@@ -108,51 +108,51 @@ let shop = (function () {
 function creatCart() {
     var cartlist = shop.carts();
     var exit = "";
-    for(var i in cartArray) {
-    let tr = document.createElement("tr")
+    for (var i in cartArray) {
+        let tr = document.createElement("tr")
 
-    let td = document.createElement("td")
-    td.textContent = cartlist[i].name
-    let td1 = document.createElement("td")
-    td1.textContent = cartlist[i].price
-    let td2 = document.createElement("td")  //3cu hisse
-    let div = document.createElement("div") //div start
-    div.className='input-group'
-    let btn = document.createElement("button")
-    btn.setAttribute("data-name=", cartArray[i].name);
-    btn.className='minus-item input-group-addon btn btn-primary'
-    btn.textContent = "-";
-    let input = document.createElement("input")
-    input.setAttribute("data-name=", cartArray[i].name);
-    input.type = 'number'
-    input.className = 'item-count form-control'
-    input.value = cartArray[i].count
-    let btn2 = document.createElement("button")
-    btn2.setAttribute("data-name=", cartArray[i].name);
-    btn2.className='plus-item btn btn-primary input-group-addon'
-    btn2.textContent="+"  //div bitdi td bitdi
-    let td3 = document.createElement("td")  //td start
-    let btn3 = document.createElement("button")
-    btn3.setAttribute("data-name=", cartArray[i].name);
-    btn3.className = 'delete-item btn btn-danger'
-    btn3.textContent = "X"
-    let td4 = document.createElement("td")
-    td4.textContent = cartlist[i].sum   //tr end
+        let td = document.createElement("td")
+        td.textContent = cartlist[i].name
+        let td1 = document.createElement("td")
+        td1.textContent = cartlist[i].price
+        let td2 = document.createElement("td")  //3cu hisse
+        let div = document.createElement("div") //div start
+        div.className = 'input-group'
+        let btn = document.createElement("button")
+        btn.setAttribute("data-name=", cartArray[i].name);
+        btn.className = 'minus-item input-group-addon btn btn-primary'
+        btn.textContent = "-";
+        let input = document.createElement("input")
+        input.setAttribute("data-name=", cartArray[i].name);
+        input.type = 'number'
+        input.className = 'item-count form-control'
+        input.value = cartArray[i].count
+        let btn2 = document.createElement("button")
+        btn2.setAttribute("data-name=", cartArray[i].name);
+        btn2.className = 'plus-item btn btn-primary input-group-addon'
+        btn2.textContent = "+"  //div bitdi td bitdi
+        let td3 = document.createElement("td")  //td start
+        let btn3 = document.createElement("button")
+        btn3.setAttribute("data-name=", cartArray[i].name);
+        btn3.className = 'delete-item btn btn-danger'
+        btn3.textContent = "X"
+        let td4 = document.createElement("td")
+        td4.textContent = cartlist[i].sum   //tr end
 
-    div.children(btn)
-    div.children(input)
-    div.children(btn2)
-    td2.children(div)
+        div.children(btn)
+        div.children(input)
+        div.children(btn2)
+        td2.children(div)
 
-    td3.children(btn3)
+        td3.children(btn3)
 
-    tr.children(td)
-    tr.children(td1)
-    tr.children(td2)
-    tr.children(td3)
-    tr.children(td4)
+        tr.children(td)
+        tr.children(td1)
+        tr.children(td2)
+        tr.children(td3)
+        tr.children(td4)
 
-    exit +=tr;
+        exit += tr;
     }
     var showCartElements = document.getElementsByClassName("show-cart");
     exit = showCartElements
@@ -163,20 +163,42 @@ function creatCart() {
 
 }
 
-const showCart = document.querySelector()
+const showCart = document.querySelector("add-to-cart")
 
 
 //- buttonuna vurdgumuzda sayin azalmasi
-showCartElements.addEventListener("click",function(e){
-    if(e.target && e.target.matches(".delete-item")){
+showCartElements.addEventListener("click", function (e) {
+    if (e.target && e.target.matches(".delete-item")) {
         let name = e.target.getAttribute("data-name")
         shop.removeCart(name)
         creatCart()
-    }   
+    }
 })
 
 
 //+ buttonuna vurdgumuzda sayin artmasi
-showCartElements.addEventListener("click", function(e){
-    
+showCartElements.addEventListener("click", function (e) {
+    if (e.target && e.target.matches("plus-item")) {
+        let name = e.target.getAttribute("data-name")
+        shop.addToCart(name)
+        creatCart()
+    }
 })
+
+
+// sebete add elemek
+showCart.addEventListener("click", function (e) {
+    e.preventDefault()
+    let name = this.getAttribute("data-name")
+    let price = this.getAttribute("data-price")
+    shop.addToCart(name,price,1)
+    creatCart()
+})
+
+
+//Clear etmek 
+showCart.addEventListener("click", function (e) {
+    shop.clearCart();
+    creatCart()
+})
+
